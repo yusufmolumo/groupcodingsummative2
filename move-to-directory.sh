6592311dc2c4faf22394741c4c9116e2e1232b38
yusufmolumo <y.molumo@alustudent.com>
2023-10-08 08:42:08 -0700

    Add Negpod_10-q1 directory and its contents

---
+++ move-to-directory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create the directory if it doesn't exist
+mkdir -p negpod_10-q1
+
+# Move the files to the directory
+mv main.sh students-list_0923.txt select-emails.sh student-emails.txt negpod_10-q1/
+
+echo "Files moved to 'negpod_10-q1' directory."
